e35526a4e4904ffc8fd10089a6edb20aa4efdeb1
Robin Monjo <robin.monjo@applidget.com>
2015-05-10 01:36:20 +0200

    psdock-ls squeleton

---
+++ psdock-ls/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Will list running containers and display essentials info")
+
+	// CONTAINER_ID PROCESS_PID COMMAND (found in proc/pid/cmdline) START_TIME
+}
